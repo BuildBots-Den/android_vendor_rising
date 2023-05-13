166f3601d5627052ce4645f8f57baf0955ec624f
kdrag0n <danny@kdrag0n.dev>
2023-05-13 09:53:48 +0000

    vendor: Build repainter app
    Change-Id: I7e3c66f629bc53f76f0d1f25bfb3ea7e07b792cc

--- config/common.mk
+++ config/common.mk
@@ -138,6 +138,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Repainter integration
+PRODUCT_PACKAGES += \
+    RepainterServicePriv
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
